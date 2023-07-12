722221993f3b146447dff0b5ed6db0dc71184210
dougw <dougwilbourne@gmail.com>
2023-07-12 16:03:03 -0400

    minor update to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pvcRegex'
+project = 'pvcMsg'
 copyright = '2023, Doug Wilbourne'
 author = 'Doug Wilbourne'
 
